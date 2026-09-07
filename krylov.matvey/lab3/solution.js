116fda3ac85a4f5b4a8dce8aef08606abebb1558
mkryloff <matwej71@gmail.com>
2026-09-07 18:51:41 +0300

    added README and format fixes

--- krylov.matvey/lab3/solution.js
+++ krylov.matvey/lab3/solution.js
@@ -1,9 +1,9 @@
 export function difference(arr1, arr2) {
-  let res = []
+  let res = [];
   for (let i = 0; i < arr1.length; i++) {
     if (!arr2.includes(arr1[i])) {
       res.push(arr1[i]);
     }
   }
   return res;
-};
+}

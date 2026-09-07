f9be477ae23d9d492a440e3e9a1eec14e7c8d5ab
mkryloff <matwej71@gmail.com>
2026-09-07 15:52:54 +0300

    My first javascript implementation

--- krylov.matvey/lab2/solution.js
+++ krylov.matvey/lab2/solution.js
@@ -1,2 +1,14 @@
-// Экспортируйте отсюда функцию с именем из контракта вашего варианта.
-export {};
+export function findLongestPalindrome(str) {
+  let maxLenPalindrome = ""
+  for (let i = 0; i < str.length; i++) {
+    for (i; j < str.length; j++) {
+      let string = str.slice(i, j);
+      if (string == string.split("").reverse().join("")) {
+        if (maxLenPalindrome.length < string.length) {
+          maxLenPalindrome = string;
+        }
+      }
+    }
+  }
+  return maxLenPalindrome;
+};

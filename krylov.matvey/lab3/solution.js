6315304df75a7b52d3e2a70dc5558e76b51c110d
mkryloff <matwej71@gmail.com>
2026-09-07 18:41:00 +0300

    first implementation added

--- krylov.matvey/lab3/solution.js
+++ krylov.matvey/lab3/solution.js
@@ -1,2 +1,9 @@
-// Экспортируйте отсюда функцию с именем из контракта вашего варианта.
-export {};
+export function difference(arr1, arr2) {
+  let res = []
+  for (let i = 0; i < arr1.length; i++) {
+    if (!arr2.includes(arr1[i])) {
+      res.push(arr1[i]);
+    }
+  }
+  return res;
+};
